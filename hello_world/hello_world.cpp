76c83800e5f4e537d5384eaf6c755fb9409cfd4d
yanrk <yanrkchina@163.com>
2025-06-29 10:24:15 +0800

    add a simplest cmake sample

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdio>
+
+#if __cplusplus >= 202302L
+    #include <print>
+#elif __cplusplus >= 202002L
+    #include <format>
+#endif
+
+int main()
+{
+#if __cplusplus >= 202302L
+    std::println("hello, world! (using std::println)");
+#elif __cplusplus >= 202002L
+    std::cout << std::format("hello, world! (using std::format)\n");
+#elif __cplusplus >= 201103L
+    std::cout << "hello, world! (using std::cout)" << std::endl;
+#else
+    std::printf("hello, world! (using printf)\n");
+#endif
+    return 0;
+}
---
+++ hello_world/readme.md
@@ -0,0 +1,13 @@
+
+---
+
+## CMake Build Command
+
+- **Windows**：
+  - `cmake -S . -B build -G "Visual Studio 17 2022"`
+  - `cmake --build build`
+- **Unix-Like**：
+  - `cmake -S . -B build -D CMAKE_BUILD_TYPE=Release`
+  - `cmake --build build`
+
+---
